31454a921e7cdb26847d14e24ae8e523c9617067
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2018-12-16 18:01:52 +0000

    Increase code coverage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createElement } from 'react';
+import { render } from 'react-dom';
 
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 import '../node_modules/normalize.css/normalize.css';
 
 import './index.css';
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
-
-registerServiceWorker();
+render(createElement(App), document.getElementById('root'));
