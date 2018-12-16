c6559ca02dd30844a5050ce2999c06ca4f554494
Mike Lowe <mikelowedev@gmail.com>
2018-12-16 18:04:42 +0000

    Increase code coverage

--- src/components/BoardSizeControls/index.jsx
+++ src/components/BoardSizeControls/index.jsx
@@ -5,28 +5,28 @@ import ControlLabel from '../ControlLabel';
 import ToggleButton from '../ToggleButton';
 
 const BoardSizeControls = (props) => {
-  const { columns, rows } = props;
+  const { columns, rows, onChange } = props;
 
   return (
     <div className="board-size-ctrls">
       <ControlLabel text="Board Size:" />
       <ToggleButton
         text="10x10"
-        onChange={() => props.onChange(10, 10)}
+        onChange={() => onChange(10, 10)}
         id="_10x10"
         checked={columns === 10 && rows === 10}
         name="board-size"
       />
       <ToggleButton
         text="20x20"
-        onChange={() => props.onChange(20, 20)}
+        onChange={() => onChange(20, 20)}
         id="_20x20"
         checked={columns === 20 && rows === 20}
         name="board-size"
       />
       <ToggleButton
         text="40x20"
-        onChange={() => props.onChange(20, 40)}
+        onChange={() => onChange(20, 40)}
         id="_40x20"
         checked={columns === 40 && rows === 20}
         name="board-size"
